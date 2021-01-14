69b24fb1375f1726ef9c1c42317e23c0ab3f0c2b
Mateusz Bartosik <mateusz@ravendb.net>
2021-01-14 13:40:26 +0100

    HRMarketing-734 Rotation of Banners

---
+++ RaccoonBlog.Web/Helpers/Banner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RaccoonBlog.Web.Helpers
+{
+    public class Banner
+    {
+        public string ResourceUrl { get; set; }
+        public string ImageSrc { get; set; }
+        public string AltText { get; set; }
+        
+    }
+}
