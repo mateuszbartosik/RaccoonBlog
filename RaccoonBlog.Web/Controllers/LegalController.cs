47951f86164e13d4c87349c585fbbf8ec915e114
Mateusz Bartosik <mateusz@ravendb.net>
2021-02-12 13:13:33 +0100

    HRINT-2094 Add Terms and Privacy Policy

---
+++ RaccoonBlog.Web/Controllers/LegalController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+
+namespace RaccoonBlog.Web.Controllers
+{
+    public class LegalController : RaccoonController
+    {
+        [HttpGet]
+        [Route("privacy-policy")]
+        public virtual ActionResult PrivacyPolicy()
+        {
+            return View("PrivacyPolicy");
+        }
+
+        [HttpGet]
+        [Route("terms")]
+        public virtual ActionResult Terms()
+        {
+            return View("Terms");
+        }
+    }
+}
